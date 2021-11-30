c568f13e98f8bddca6b00bb466c9ca9feec33eeb
Mark <46990472+uppzz@users.noreply.github.com>
2021-11-30 06:01:37 +0100

    Update open.js

--- controllers/open.js
+++ controllers/open.js
@@ -2,6 +2,7 @@ exports.getIndex = (req, res, next) => {
    res.render("open/index", {
       layout: "base",            // default  Boolean false    auch möglich
       helper: {},                // können hier auch individuel überschrieben werden
-      pageTitle: "Programmierhilfe.de"
+      pageTitle: "Programmierhilfe.de",
+      erfundeneVariable: req.erfundeneVariable
    });
 };
