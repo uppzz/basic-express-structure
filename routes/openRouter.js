e99c98e0b88bfbfb1bd0ec8e87c54262216cbc44
Mark <46990472+uppzz@users.noreply.github.com>
2021-12-02 15:17:06 +0100

    Update openRouter.js

--- routes/openRouter.js
+++ routes/openRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const openController = require('../controllers/openController.js');
+const openController = require('../controllers/open/openController.js');
 // getIndex wird nur aufgerufen, wenn bei isTrue next() aufgerufen wird
 //* usecase  check if user is logged in  or have the privileges to access the getIndex side
 router.get("/", openController.isTrue, openController.getIndex);
